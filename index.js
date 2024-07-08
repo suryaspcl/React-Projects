c38be3386a64c6ab5bde3c16ca0ade16e9466a7f
suryspcl <sury295.spcl@gmail.com>
2024-07-08 22:40:53 +0530

    starting changes

---
+++ index.js
@@ -0,0 +1,15 @@
+
+ 
+ var element = document.querySelector(".myClass")
+ console.log(element.textContent);
+
+
+
+ var elements = document.querySelectorAll(".myClass")
+ elements.forEach(function(element) {
+
+ 
+    console.log(element.textContent);
+ });
+ 
+
